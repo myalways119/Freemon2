7eb8a03bd32566176ea2a60fccd4dd4fde0dfe4b
Ho <myalways119>
2021-11-02 23:20:48 +0900

    1

--- app/src/main/java/activity/IntroActivity.kt
+++ app/src/main/java/activity/IntroActivity.kt
@@ -1,6 +1,9 @@
 package activity
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -8,7 +11,10 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import co.kr.freemon2.R
+import android.telephony.TelephonyManager
+import android.widget.Toast
 
 class IntroActivity : AppCompatActivity() {
 
@@ -18,17 +24,38 @@ class IntroActivity : AppCompatActivity() {
 
     private lateinit var introImage : ImageView
     private lateinit var introText : TextView
+    private var devicePhoneNum:String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        IntroAnimation()*
+        devicePhoneNum = GetDeviceInfo()
+        Toast.makeText(this, devicePhoneNum, Toast.LENGTH_LONG).show()
+
+        IntroAnimation()
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }, SPLASH_SCREEN.toLong())
+    }
+
+    fun GetDeviceInfo():String
+    {
+        var devicePhoneNum:String = ""
+
+        // READ_PHONE_NUMBERS 또는 READ_PHONE_STATE 권한을 허가 받았는지 확인
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)
+        {
+            //val tm = getSystemService<Any>(Context.TELEPHONY_SERVICE) as TelephonyManager
+            //tm.line1Number();
+
+            var teleManager: TelephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            devicePhoneNum = teleManager.line1Number.toString()
+        }
+
+        return devicePhoneNum
     }
 
     fun IntroAnimation()
