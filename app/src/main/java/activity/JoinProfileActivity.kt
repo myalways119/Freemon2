2e32d4c6a15ef0f2e79fd238b8cfff6293971b04
20210025 <myalways119@naver.com>
2021-11-04 16:26:20 +0900

    232222

--- app/src/main/java/activity/JoinProfileActivity.kt
+++ app/src/main/java/activity/JoinProfileActivity.kt
@@ -1,12 +1,34 @@
 package activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
 import co.kr.freemon2.R
 
-class JoinProfileActivity : AppCompatActivity() {
+class JoinProfileActivity : AppCompatActivity()
+{
+    private val REQUEST_PICK_IMAGE = 2
+
+
+    var imgProfileCamera = findViewById<ImageView>(R.id.joinProfile_imageViewIcon)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_profile)
+
+        imgProfileCamera.setOnClickListener(openGallery)
     }
+
+    private fun openGallery() {
+        Intent(Intent.ACTION_GET_CONTENT).also { intent ->
+            intent.type = "image/*"
+            intent.resolveActivity(packageManager)?.also {
+                startActivityForResult(intent, REQUEST_PICK_IMAGE)
+            }
+        }
+    }
+
 }
--- app/src/main/java/common/CommonConst.kt
+++ app/src/main/java/common/CommonConst.kt
@@ -7,7 +7,8 @@ import androidx.core.content.ContextCompat
 object CommonConst
 {
     public val requiredPermissions = arrayOf(
-        Manifest.permission.READ_PHONE_STATE
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
     object DbResponseCode
